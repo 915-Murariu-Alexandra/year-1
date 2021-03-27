884cb73e1a9f51bc5d0b163a8b38f242dd82ea01
915-Murariu-Alexandra <nymeriankida@yahoo.com>
2021-03-27 22:54:30 +0200

    no ui or specs

--- Semester 2/Graph Algorithms/Homework-1/cpp_graph/Graph.cpp
+++ Semester 2/Graph Algorithms/Homework-1/cpp_graph/Graph.cpp
@@ -1,16 +1,15 @@
 //
 // Created by kida on 3/27/21.
 //
-
+#define MAX_COST 20
 #include "Graph.h"
 #include <fstream>
 #include <iostream>
-#include <exception>
-
+#include <random>
 void Graph::read_from_file(const std::string &name) {
     std::ifstream new_file;
     new_file.open(name.c_str());
-    if(new_file.is_open()){
+    if (new_file.is_open()) {
         int n, m;
         new_file >> n >> m;
         this->nr_of_vertices = n;
@@ -20,54 +19,141 @@ void Graph::read_from_file(const std::string &name) {
             int cost;
             new_file >> node_1 >> node_2 >> cost;
             auto new_edge = std::pair<node, node>(node_1, node_2);
-            this->dict_in.insert({node_2, node_1});
-            this->dict_out.insert({node_1, node_2});
+            this->dict_in[node_2].push_back(node_1);
+            this->dict_out[node_1].push_back(node_2);
             this->dict_cost.insert({new_edge, cost});
         }
         new_file.close();
-    }
-    else {
-        throw std::exception();
+    } else {
+        throw std::runtime_error("The file doesn't exist");
     }
 }
 
 void Graph::write_to_file(const std::string &name) {
     std::fstream new_file;
     new_file.open(name.c_str(), std::ios::out);
-    if(new_file.is_open()){
+    if (new_file.is_open()) {
         new_file << std::to_string(this->nr_of_vertices) << " " << std::to_string(this->nr_of_edges) << "\n";
-        for(std::pair<edge, int> element : this->dict_cost) {
+        for (std::pair<edge, int> element : this->dict_cost) {
             new_file << element.first.first << " " << element.first.second << " " << element.second << "\n";
         }
-    }
-    else {
-        throw std::exception();
+    } else {
+        throw std::runtime_error("The file couldn't be opened");
     }
 }
 
 bool Graph::is_edge(node node_1, node node_2) {
-    if(this->dict_cost.count(std::pair<node, node>(node_1, node_2)) != 0) {
+    if (this->dict_cost.count(std::pair<node, node>(node_1, node_2)) != 0) {
         return true;
     }
     return false;
 }
 
 int Graph::in_degree(node vertex) {
-    int degree = 0;
-    for(std::pair<node, node> element: this->dict_in) {
-        if(element.first == vertex) {
-            degree++;
-        }
+    if (dict_in.count(vertex) != 0) {
+        return dict_in.at(vertex).size();
     }
-    return degree;
+    return 0;
 }
 
 int Graph::out_degree(node vertex) {
-    int degree = 0;
-    for(std::pair<node, node> element: this->dict_out) {
-        if(element.first == vertex) {
-            degree++;
+    if (dict_out.count(vertex) != 0) {
+        return dict_out.at(vertex).size();
+    }
+    return 0;
+}
+
+void Graph::modify_cost(node node_1, node node_2, int new_cost) {
+    if (!is_edge(node_1, node_2)) {
+        throw std::runtime_error("The edge doesn't exist");
+    } else {
+        this->dict_cost[std::pair<node, node>(node_1, node_2)] = new_cost;
+    }
+}
+
+void Graph::delete_edge(node node_1, node node_2) {
+    if (!is_edge(node_1, node_2)) {
+        throw std::runtime_error("The edge doesn't exist");
+    } else {
+        this->dict_cost.erase(std::pair<node, node>(node_1, node_2));
+        for (auto it = this->dict_in[node_2].begin(); it != this->dict_in[node_2].end(); ++it) {
+            if (*it == node_1) {
+                this->dict_in[node_2].erase(it);
+                it--;
+            }
+        }
+        for (auto it = this->dict_out[node_1].begin(); it != this->dict_out[node_1].end(); ++it) {
+            if (*it == node_2) {
+                this->dict_out[node_1].erase(it);
+                it--;
+            }
+        }
+        this->nr_of_edges--;
+    }
+}
+
+void Graph::add_edge(node node_1, node node_2, int cost) {
+    if (is_edge(node_1, node_2)) {
+        throw std::runtime_error("The edge already exists");
+    } else if (node_1 < 0 || node_1 > this->nr_of_vertices || node_2 < 0 || node_2 > this->nr_of_vertices) {
+        throw std::runtime_error("The vertices are invalid");
+    } else {
+        this->dict_cost.insert({std::pair<node, node>(node_1, node_2), cost});
+        this->dict_in[node_2].push_back(node_1);
+        this->dict_out[node_1].push_back(node_2);
+    }
+}
+
+void Graph::add_node() {
+    this->nr_of_vertices += 1;
+}
+
+void Graph::delete_node(node vertex) {
+    if (vertex < 0 || vertex > this->nr_of_vertices) {
+        throw std::runtime_error("The vertex to remove is not valid.");
+    } else {
+        this->dict_out[vertex].clear();
+
+        for (std::pair<node, std::vector<node>> element : this->dict_in) {
+            for (auto it = element.second.begin(); it != element.second.end();) {
+                if (*it == vertex) {
+                    element.second.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+        }
+
+        for (auto it = this->dict_cost.begin(); it != this->dict_cost.end();) {
+            if (it->first.first == vertex || it->first.second == vertex) {
+                it = this->dict_cost.erase(it);
+            } else {
+                ++it;
+            }
         }
     }
-    return degree;
 }
+
+Graph Graph::random_graph(int n, int m) {
+    Graph g;
+    node node_1, node_2;
+    unsigned int cost;
+    g.nr_of_edges = n;
+    g.nr_of_vertices = m;
+    if(m > n*(n-1)){
+        throw std::runtime_error("Such a graph cannot be composed.");
+    }
+    while(m > 0) {
+        std::random_device rd;
+        node_1 = rd() % n;
+        node_2 = rd() % n;
+        cost = rd() % MAX_COST;
+        if(!g.is_edge(node_1, node_2)) {
+            g.add_edge(node_1, node_2, cost);
+            m--;
+        }
+    }
+    return g;
+}
+
+
--- Semester 2/Graph Algorithms/Homework-1/cpp_graph/Graph.h
+++ Semester 2/Graph Algorithms/Homework-1/cpp_graph/Graph.h
@@ -12,27 +12,63 @@
 
 typedef int node;
 typedef std::pair<node, node> edge;
+
 class Graph {
 private:
     int nr_of_vertices{};
     int nr_of_edges{};
-    std::multimap<node, node> dict_in;
-    std::multimap<node, node> dict_out;
+    std::map<node, std::vector<node>> dict_in;
+    std::map<node, std::vector<node>> dict_out;
     std::map<edge, int> dict_cost;
 public:
     Graph() = default;
-    int get_nr_of_vertices() const {return this->nr_of_vertices;};
-    std::multimap<node, node> get_dict_in() {return this->dict_in;};
-    std::multimap<node, node> get_dict_out() {return this->dict_out;};
-    std::map<edge, int> get_dict_cost() {return this->dict_cost;};
-    std::map<node, node>::iterator get_dict_in_iterator(){auto it = this->dict_in.begin();return it;};
-    std::map<node, node>::iterator get_dict_out_iterator(){auto it = this->dict_out.begin();return it;};
-    std::map<edge, int>::iterator get_dict_cost_iterator(){auto it = this->dict_cost.begin();return it;};
-    void read_from_file(const std::string&);
-    void write_to_file(const std::string&);
+
+    int get_nr_of_vertices() const { return this->nr_of_vertices; };
+
+    std::map<node, std::vector<node>> get_dict_in() { return this->dict_in; };
+
+    std::map<node, std::vector<node>> get_dict_out() { return this->dict_out; };
+
+    std::map<edge, int> get_dict_cost() { return this->dict_cost; };
+
+    auto get_dict_in_iterator(node vertex) {
+        auto it = this->dict_in.at(vertex).begin();
+        return it;
+    };
+
+    auto get_dict_out_iterator(node vertex) {
+        auto it = this->dict_out.at(vertex).begin();
+        return it;
+    };
+
+    std::map<edge, int>::iterator get_dict_cost_iterator() {
+        auto it = this->dict_cost.begin();
+        return it;
+    };
+
+    void read_from_file(const std::string &);
+
+    void write_to_file(const std::string &);
+
     bool is_edge(node, node);
+
     int in_degree(node);
+
     int out_degree(node);
+
+    void modify_cost(node, node, int);
+
+    void delete_edge(node, node);
+
+    void add_edge(node, node, int);
+
+    void add_node();
+
+    void delete_node(node);
+
+    Graph(const Graph &g) { nr_of_vertices = g.get_nr_of_vertices(), nr_of_edges = g.nr_of_edges, dict_in = g.dict_in,
+                            dict_out = g.dict_out, dict_cost = g.dict_cost; };
+    static Graph random_graph(int, int);
 };
 
 
